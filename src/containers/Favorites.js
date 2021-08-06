a80219f4ace7e8a6d1694001ff0f5b967dd2053f
Adeline Lefebvre <adelinelefebvre@macbook-pro-de-adeline.home>
2021-08-06 12:42:12 +0200

    message

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,45 +1,50 @@
 import "../App.scss";
 import { useHistory } from "react-router-dom";
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
 
 const Favorites = ({ favCharacters, favComics }) => {
   const history = useHistory();
-  const favComicsCookies = Cookies.get("favComics");
-  const favCharactersCookies = Cookies.get("favCharacters");
+  // const favComicsCookies = Cookies.get("favComics");
+  // const favCharactersCookies = Cookies.get("favCharacters");
 
   return (
     <div>
       <div className="title">Comics Favoris</div>
-      {favComicsCookies &&
-        favComicsCookies.map((comic) => {
-          return (
-            <div className="comic" key={comic._id}>
-              <div>{comic.title}</div>
-              <img src={`${comic.thumbnail.path}.jpg`} alt="" />
-              <div className="description">{comic.description}</div>
-            </div>
-          );
-        })}
+      <div className="comics">
+        {favComics &&
+          favComics.map((comic) => {
+            return (
+              <div className="comic" key={comic._id}>
+                <div>{comic.title}</div>
+                <img src={`${comic.thumbnail.path}.jpg`} alt="" />
+                <div className="description">{comic.description}</div>
+              </div>
+            );
+          })}
+      </div>
+
       <div className="title">Personnages favoris</div>
-      {favCharactersCookies &&
-        favCharactersCookies.map((character) => {
-          return (
-            <div
-              className="character"
-              key={character._id}
-              onClick={() => {
-                history.push({
-                  pathname: `/character/${character._id}`,
-                  state: { character: character },
-                });
-              }}
-            >
-              <div className="name">{character.name}</div>
-              <img src={`${character.thumbnail.path}.jpg`} alt="" />
-              <div className="description">{character.description}</div>
-            </div>
-          );
-        })}
+      <div className="characters">
+        {favCharacters &&
+          favCharacters.map((character) => {
+            return (
+              <div
+                className="character"
+                key={character._id}
+                onClick={() => {
+                  history.push({
+                    pathname: `/character/${character._id}`,
+                    state: { character: character },
+                  });
+                }}
+              >
+                <div className="name">{character.name}</div>
+                <img src={`${character.thumbnail.path}.jpg`} alt="" />
+                <div className="description">{character.description}</div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
